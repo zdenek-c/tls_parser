1be0b2eddcfc2f2f4099918008645e32c6dc5042
Zdenek Chovanec <xchova19@stud.fit.vutbr.cz>
2020-03-16 11:24:31 +0100

    Refator parser

--- tls_parser/parser.py
+++ tls_parser/parser.py
@@ -1,25 +1,26 @@
+from typing import Tuple
+
 from tls_parser.alert_protocol import TlsAlertRecord
 from tls_parser.exceptions import UnknownTypeByte
 from tls_parser.handshake_protocol import TlsHandshakeRecord
-from tls_parser.record_protocol import TlsRecord, TlsRecordHeader, TlsRecordTypeByte
-from typing import Tuple
+from tls_parser.record_protocol import TlsRecord
+from tls_parser.record_protocol import TlsRecordHeader
+from tls_parser.record_protocol import TlsRecordTypeByte
 
 
 class TlsRecordParser(object):
 
     @staticmethod
-    def parse_bytes(raw_bytes):
-        # type: (bytes) -> Tuple[TlsRecord, int]
+    def parse_bytes(raw_bytes: bytes) -> Tuple[TlsRecord, int]:
         record_header, len_consumed = TlsRecordHeader.from_bytes(raw_bytes)
 
         # Try to parse the record
         if record_header.type == TlsRecordTypeByte.HANDSHAKE:
             return TlsHandshakeRecord.from_bytes(raw_bytes)
-        elif record_header.type == TlsRecordTypeByte.ALERT:
+        if record_header.type == TlsRecordTypeByte.ALERT:
             return TlsAlertRecord.from_bytes(raw_bytes)
-        elif record_header.type in TlsRecordTypeByte:
+        if record_header.type in TlsRecordTypeByte:
             # Valid record type but we don't have the code to parse it right now
             return TlsRecord.from_bytes(raw_bytes)
-        else:
-            # Unknown type
-            raise UnknownTypeByte()
+        # Unknown type
+        raise UnknownTypeByte()
