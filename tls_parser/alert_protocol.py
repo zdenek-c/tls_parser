b7ebe3c3f6f8507f838bcfc243a72b8bcc5395a1
Zdenek Chovanec <xchova19@stud.fit.vutbr.cz>
2020-03-16 15:40:06 +0100

    Refactor alert_protocol

--- tls_parser/alert_protocol.py
+++ tls_parser/alert_protocol.py
@@ -1,12 +1,14 @@
-from __future__ import absolute_import
-from __future__ import print_function
-
 import struct
 from enum import IntEnum
-from tls_parser.exceptions import NotEnoughData, UnknownTypeByte
-from tls_parser.record_protocol import TlsSubprotocolMessage, TlsRecord, TlsRecordHeader, TlsRecordTypeByte
-from tls_parser.tls_version import TlsVersionEnum
 from typing import Tuple
+
+from tls_parser.exceptions import NotEnoughData
+from tls_parser.exceptions import UnknownTypeByte
+from tls_parser.record_protocol import TlsRecord
+from tls_parser.record_protocol import TlsRecordHeader
+from tls_parser.record_protocol import TlsRecordTypeByte
+from tls_parser.record_protocol import TlsSubprotocolMessage
+from tls_parser.tls_version import TlsVersionEnum
 
 
 class TlsAlertSeverityByte(IntEnum):
@@ -16,38 +18,41 @@ class TlsAlertSeverityByte(IntEnum):
 
 class TlsAlertMessage(TlsSubprotocolMessage):
 
-    def __init__(self, alert_severity, alert_description):
-        # type: (TlsAlertSeverityByte, int) -> None
+    def __init__(
+            self,
+            alert_severity: TlsAlertSeverityByte,
+            alert_description: int) -> None:
         self.alert_severity = alert_severity
-        # Right now the description is just stored as an int instead of a TlsAlertDescriptionByte
+        # Right now the description is just stored as an int instead
+        # of a TlsAlertDescriptionByte
         self.alert_description = alert_description
 
     @classmethod
-    def from_bytes(cls, raw_bytes):
-        # type: (bytes) -> Tuple[TlsAlertMessage, int]
+    def from_bytes(cls, raw_bytes: bytes) -> Tuple["TlsAlertMessage", int]:
         if len(raw_bytes) < 2:
             raise NotEnoughData()
-        
-        alert_severity = TlsAlertSeverityByte(struct.unpack('B', raw_bytes[0:1])[0])
-        alert_description = struct.unpack('B', raw_bytes[1:2])[0]
+
+        alert_severity = TlsAlertSeverityByte(
+            struct.unpack("B", raw_bytes[0:1])[0])
+        alert_description = struct.unpack("B", raw_bytes[1:2])[0]
         return TlsAlertMessage(alert_severity, alert_description), 2
 
-    def to_bytes(self):
-        # type: () -> bytes
-        bytes = b''
-        bytes += struct.pack('B', self.alert_severity.value)
-        bytes += struct.pack('B', self.alert_description)
-        return bytes
+    def to_bytes(self) -> bytes:
+        bytes_array = b""
+        bytes_array += struct.pack("B", self.alert_severity.value)
+        bytes_array += struct.pack("B", self.alert_description)
+        return bytes_array
 
 
 class TlsAlertRecord(TlsRecord):
-    def __init__(self, record_header, alert_message):
-        # type: (TlsRecordHeader, TlsAlertMessage) -> None
+    def __init__(
+            self,
+            record_header: TlsRecordHeader,
+            alert_message: TlsAlertMessage) -> None:
         super(TlsAlertRecord, self).__init__(record_header, [alert_message])
 
     @property
-    def alert_severity(self):
-        # type: () -> TlsAlertSeverityByte
+    def alert_severity(self) -> TlsAlertSeverityByte:
         """Convenience method to get the severity of the underlying Alert message.
 
         This makes the assumption that an Alert record only contains one Alert message, which seems to be the case in
@@ -56,8 +61,7 @@ class TlsAlertRecord(TlsRecord):
         return self.subprotocol_messages[0].alert_severity
 
     @property
-    def alert_description(self):
-        # type: () -> int
+    def alert_description(self) -> int:
         """Convenience method to get the description of the underlying Alert message.
 
         This makes the assumption that an Alert record only contains one Alert message, which seems to be the case in
@@ -66,20 +70,24 @@ class TlsAlertRecord(TlsRecord):
         return self.subprotocol_messages[0].alert_description
 
     @classmethod
-    def from_parameters(cls, tls_version, alert_severity, alert_description):
-        # type: (TlsVersionEnum, TlsAlertSeverityByte, int) -> TlsAlertRecord
+    def from_parameters(
+            cls,
+            tls_version: TlsVersionEnum,
+            alert_severity: TlsAlertSeverityByte,
+            alert_description: int) -> "TlsAlertRecord":
         alert_message = TlsAlertMessage(alert_severity, alert_description)
-        record_header = TlsRecordHeader(TlsRecordTypeByte.ALERT, tls_version, alert_message.size)
+        record_header = TlsRecordHeader(
+            TlsRecordTypeByte.ALERT, tls_version, alert_message.size)
         return TlsAlertRecord(record_header, alert_message)
 
     @classmethod
-    def from_bytes(cls, raw_bytes):
-        # type: (bytes) -> Tuple[TlsAlertRecord, int]
+    def from_bytes(cls, raw_bytes: bytes) -> Tuple["TlsAlertRecord", int]:
         header, len_consumed = TlsRecordHeader.from_bytes(raw_bytes)
         remaining_bytes = raw_bytes[len_consumed::]
 
         if header.type != TlsRecordTypeByte.ALERT:
             raise UnknownTypeByte()
 
-        message, len_consumed_for_message = TlsAlertMessage.from_bytes(remaining_bytes)
+        message, len_consumed_for_message = TlsAlertMessage.from_bytes(
+            remaining_bytes)
         return TlsAlertRecord(header, message), len_consumed + len_consumed_for_message
